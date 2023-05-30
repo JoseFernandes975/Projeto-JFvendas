8f23ff72102bce9a0c64668d1ba82051fd32acd8
JoseFernandes975 <zehenrique752005@gmail.com>
2023-05-30 15:06:38 -0300

    Project clean

---
+++ backend/src/test/java/com/josefernandes/jfvendas/JfvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josefernandes.jfvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JfvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div>
+      <h1>Bem Vindo ao JFVendas</h1>
+    </div>
+  );
+}
+
+export default App;
