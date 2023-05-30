3d990351d689a7efcfa5c67459ec4fe96cea0b5f
JoseFernandes975 <zehenrique752005@gmail.com>
2023-05-30 17:08:10 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+function Footer() {
+    return (
+     <div>
+<footer className="footer mt-auto py-3 bg-dark">
+  <div className="container">
+    <p className="text-light">App desenvolvido por <a href="https://github.com/JoseFernandes975" target="_blank" rel="noreferrer">José Fernandes</a></p>
+    <p className="text-light"><small><strong>JFVendas</strong><br/>
+     Instagram: <a href="https://instagram.com/jose__975" target="_blank" rel="noreferrer">@jose__975</a></small></p>
+  </div>
+</footer>
+     </div>
+    );
+}
+
+export default Footer;
